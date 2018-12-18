cbb5e9c51f8c2d2b07e893d025b979bb790575f3
yusihui <yusihui@geotmt.com>
2018-12-18 20:35:16 +0800

    dev分支新加入class

---
+++ src/main/java/ks/KuaiShouC2.java
@@ -0,0 +1,15 @@
+package ks;
+
+import ks.impl.Kuaishou;
+
+public class KuaiShouC2 implements Kuaishou {
+    @Override
+    public String getAge() {
+        return "55";
+    }
+
+    @Override
+    public void setAge() {
+
+    }
+}
