f95bb93d0442d7ca746cc3d504d01ba7346732ba
yusihui <yusihui@geotmt.com>
2018-12-18 20:24:32 +0800

    kstmt 项目提交

---
+++ src/main/java/ks/KuaiShouC.java
@@ -0,0 +1,9 @@
+package ks;
+
+import ks.impl.Kuaishou;
+
+public class KuaiShouC implements Kuaishou {
+    public String getAge() {
+        return "32";
+    }
+}
---
+++ src/main/java/ks/impl/Kuaishou.java
@@ -0,0 +1,5 @@
+package ks.impl;
+
+public interface Kuaishou {
+    public String  getAge();
+}
